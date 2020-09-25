74d3b86e0f69c69a39cc52bd5e21040d5123c3de
David K <45532116+iactivevision@users.noreply.github.com>
2020-09-25 20:36:50 +0300

    sd

--- src/components/AnalyticChartWidget.js
+++ src/components/AnalyticChartWidget.js
@@ -3,8 +3,20 @@ import React from "react";
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts";
 
+const options: Highcharts.Options = {
+  title: {
+    text: "My chart"
+  },
+  series: [
+    {
+      type: "line",
+      data: [1, 2, 3]
+    }
+  ]
+};
+
 export default class AnalyticChartWidget extends React.Component {
   render() {
-    return <HighchartsReact highcharts={highcharts} option={options} />;
+    return <HighchartsReact highcharts={Highcharts} option={options} />;
   }
 }
